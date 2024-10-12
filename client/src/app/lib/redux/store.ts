84bf5888ee90610ee08525dd49958ed232f3e1cd
roberto <tkp1@hanmail.net>
2024-10-12 11:44:16 +0900

    [feat] : store 타입 추가 (useAppDispatch, useAppSelector)

--- client/src/app/lib/redux/store.ts
+++ client/src/app/lib/redux/store.ts
@@ -1,8 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { persistReducer } from "redux-persist";
-import rootReducer from "./rootReducer"; // 루트 리듀서 가져오기
+import rootReducer from "./rootReducer";
 import { api } from "@/app/lib/redux/state/api";
 import { persistConfig } from "@/app/lib/redux/persist-config";
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+} from "redux-persist/es/constants";
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
@@ -31,3 +40,5 @@ export const makeStore = () => {
 export type AppStore = ReturnType<typeof makeStore>;
 export type RootState = ReturnType<AppStore["getState"]>;
 export type AppDispatch = AppStore["dispatch"];
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
